70d2109e4b567cc302332a2150fd40dd135b14fc
ms5788 <ms5788@drexel.edu>
2026-04-06 23:40:14 -0400

    make the match the instruction closely

--- src/AlarmClockRadio/main.java
+++ src/AlarmClockRadio/main.java
@@ -7,24 +7,25 @@ public class main
     public static void main(String[] args)
     {
         int i;
-        int seconds;
-        LocalTime alarmTime = LocalTime.of(10,0);
-        AlarmClock myAlarmClock = new AlarmClock(alarmTime, true);
+        LocalTime alarmTime = LocalTime.of(8,0);
+        LocalTime currentTime = LocalTime.of(8,0);
+        AlarmClock myAlarmClock = new AlarmClock(alarmTime, true, currentTime);
 
         for (i = 0; i < 5; i++)
         {
-            System.out.println("Time: " + myAlarmClock.getAlarm());
+            System.out.println("Time: " + myAlarmClock.getCurrentTime());
 
             for (int seconds = 0; seconds < 60; seconds++)
             {
                 myAlarmClock.getIsAlarmOn();
-                myAlarmClock.;
+                myAlarmClock.tick();
             }
         }
         myAlarmClock.snooze();
+
         for (i = 0; i < 9; i++)
         {
-            System.out.println("Time: " + myAlarmClock.getAlarm());
+            System.out.println("Time: " + myAlarmClock.getCurrentTime());
             for (int seconds = 0; seconds < 60; seconds++)
             {
                 myAlarmClock.getIsAlarmOn();
